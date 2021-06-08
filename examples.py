2fa0b1a11975acd5198783766f32311cb2752348
Kyle Pounder <k.pounder@gmail.com>
2021-06-07 21:05:18 -0700

    Improved examples

--- examples.py
+++ examples.py
@@ -6,25 +6,30 @@ import seaborn as sns
 import graphing.custom_seaborn as csns
 
 
+# Sample data for graphs 
 l = len(pd.date_range(start=datetime.date(2018,1,1), end=datetime.date(2018,7,1), freq='D'))
 test_data = pd.DataFrame(
     data={
         'x': np.arange(1, l+1, step=1),
         'y': 1/(2*l) * np.arange(1, l+1, step=1),
         'd': pd.date_range(start=datetime.date(2018,1,1), end=datetime.date(2018,7,1), freq='D'),
-        'z': np.exp(-1*np.arange(1, l+1, step=1))
+        'z': np.exp(-1*np.arange(1, l+1, step=1)),
+        'a': 5/(l) * np.arange(1, l+1, step=1)
     }
 )
 
+
+# Basic line plot
 fig, ax = plt.subplots(figsize=(10,5))
 sns.lineplot(
-    x='x', y='y', data=test_data.iloc[:5, :], err_style=None, linewidth=5, ax=ax
+    x='x', y='y', data=test_data.iloc[:20, :], err_style=None, linewidth=5, ax=ax
 )
 ax = csns.customize_axis(
     ax=ax
 )
 plt.show()
 
+# Line plot with custom x axis label, ticks, and tick marks 
 fig, ax = plt.subplots(figsize=(10,5))
 sns.lineplot(
     x='d', y='y', data=test_data, err_style=None, linewidth=5, ax=ax
@@ -38,9 +43,10 @@ ax = csns.customize_axis(
 )
 plt.show()
 
+# Scatter plot 
 fig, ax = plt.subplots(figsize=(10,5))
 sns.scatterplot(
-    x='x', y='y', data=test_data.iloc[:10, :], ax=ax
+    x='x', y='a', data=test_data.iloc[:5, :], ax=ax
 )
 ax = csns.customize_axis(
     ax=ax,
@@ -49,12 +55,13 @@ ax = csns.customize_axis(
     ylabel='Y Axis Label',
     xticks=np.arange(1, 6, step=1),
     yticks=np.arange(0, 1.1, step=0.1),
-    xticklabels=['One', 'Two', 'Three', 'Four', 'Five'],
+    xticklabels=[1, 2, 3, 4, 5], #['One', 'Two', 'Three', 'Four', 'Five'],
     yticklabels=[f"{100*y:0.1f}%" for y in np.arange(0, 1.1, step=0.1)],
     spines={'top': False, 'right': False, 'bottom': 'grey', 'left': 'grey'}
 )
 plt.show()
 
+# Two stacked graphs (subplots)
 fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(10,8), gridspec_kw={'height_ratios': [1, 3], 'hspace': 0.15})
 sns.lineplot(
     x='x', y='y', data=test_data, color='grey', err_style=None, linewidth=3, ax=ax[0]
@@ -78,6 +85,7 @@ ax[1] = csns.customize_axis(
 )
 plt.show()
 
+# Bar plot 
 fig, ax = plt.subplots(figsize=(10,5))
 sns.barplot(
     x='d', y='z', data=test_data.iloc[:5, :], color='lightgrey', ax=ax
@@ -92,13 +100,13 @@ ax.patches[0].set_color('steelblue')
 ax.patches[-1].set_color('salmon')
 plt.show()
 
-
+# Overlayed histograms 
 fig, ax = plt.subplots(figsize=(10,8))
 sns.distplot(
-    a=test_data['z'], bins=10, kde=False, norm_hist=False, color='grey', label='Control', ax=ax
+    a=test_data['z'], bins=20, kde=False, norm_hist=False, color='grey', label='Control', ax=ax
 )
 sns.distplot(
-    a=test_data['y'], bins=10, kde=False, norm_hist=False, color='steelblue', label='Test', hist_kws=dict(alpha=0.25), ax=ax
+    a=test_data['y'], bins=20, kde=False, norm_hist=False, color='steelblue', label='Test', hist_kws=dict(alpha=0.25), ax=ax
 )
 ax = csns.customize_axis(
     ax=ax,
