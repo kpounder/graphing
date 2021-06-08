f21f5d1c59421f6c414d6d965a506622b66c46e2
Kyle Pounder <k.pounder@gmail.com>
2021-06-07 20:32:46 -0700

    first commit

---
+++ custom_seaborn.py
@@ -0,0 +1,65 @@
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+import seaborn as sns
+import yaml
+
+
+with open('/Users/kpounder/Documents/code/graphing/preferences.yaml') as file:
+    PREFS = yaml.load(file, Loader=yaml.FullLoader)
+
+
+def customize_axis(
+    ax,
+    grid=False,
+    title='',
+    xlabel='',
+    ylabel='',
+    showxlabel=True,
+    showylabel=True,
+    xticks=None,
+    yticks=None,
+    xticklabels=None,
+    yticklabels=None,
+    spines={'top': False, 'right': False, 'bottom': 'grey', 'left': 'grey'},
+    legend=False
+):
+    if grid:
+        ax.grid(axis='both', **PREFS['grid'])
+    else:
+        ax.grid(False)
+
+    if title:
+        ax.set_title(title, **PREFS['title'])
+
+    ax.set_xlabel(xlabel, **PREFS['axislabels'], **PREFS['xaxislabel'])
+    ax.set_ylabel(ylabel, **PREFS['axislabels'], **PREFS['yaxislabel'])
+
+    if xticks is not None:
+        ax.set_xticks(xticks)
+    if yticks is not None:
+        ax.set_yticks(yticks)
+    if xticklabels is not None:
+        ax.set_xticklabels(xticklabels)
+    if yticklabels is not None:
+        ax.set_yticklabels(yticklabels)
+
+    if showxlabel and showylabel:
+        ax.tick_params(axis='both', **PREFS['axisticks'])
+    elif showylabel and (not showxlabel):
+        ax.tick_params(axis='x', bottom=False, labelbottom=False, **PREFS['axisticks'])
+        ax.tick_params(axis='y', **PREFS['axisticks'])
+
+    for side in spines.keys():
+        if type(spines[side]) == str:
+            ax.spines[side].set_color(spines[side])
+        else:
+            ax.spines[side].set_visible(spines[side])
+
+    if legend:
+        ax.legend()
+        l = ax.get_legend()
+        for text in l.get_texts():
+            text.set_color('grey')
+
+    return ax
---
+++ examples.py
@@ -0,0 +1,106 @@
+import graphing.custom_seaborn as csns
+import numpy as np
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+l = len(pd.date_range(start=datetime.date(2018,1,1), end=datetime.date(2018,7,1), freq='D'))
+test_data = pd.DataFrame(
+    data={
+        'x': np.arange(1, l+1, step=1),
+        'y': 1/(2*l) * np.arange(1, l+1, step=1),
+        'd': pd.date_range(start=datetime.date(2018,1,1), end=datetime.date(2018,7,1), freq='D'),
+        'z': np.exp(-1*np.arange(1, l+1, step=1))
+    }
+)
+
+fig, ax = plt.subplots(figsize=(10,5))
+sns.lineplot(
+    x='x', y='y', data=test_data.iloc[:5, :], err_style=None, linewidth=5, ax=ax
+)
+ax = csns.customize_axis(
+    ax=ax
+)
+plt.show()
+
+fig, ax = plt.subplots(figsize=(10,5))
+sns.lineplot(
+    x='d', y='y', data=test_data, err_style=None, linewidth=5, ax=ax
+)
+ax = csns.customize_axis(
+    ax=ax,
+    grid=True,
+    xlabel='Date (Quarter)',
+    xticks=pd.date_range(start=datetime.date(2018,1,1), end=datetime.date(2018,7,1), freq='QS'),
+    xticklabels=pd.date_range(start=datetime.date(2018,1,1), end=datetime.date(2018,7,1), freq='QS').map(lambda dt: dt.strftime('%b %y'))
+)
+plt.show()
+
+fig, ax = plt.subplots(figsize=(10,5))
+sns.scatterplot(
+    x='x', y='y', data=test_data.iloc[:10, :], ax=ax
+)
+ax = csns.customize_axis(
+    ax=ax,
+    title='My Title',
+    xlabel='X Axis Label',
+    ylabel='Y Axis Label',
+    xticks=np.arange(1, 6, step=1),
+    yticks=np.arange(0, 1.1, step=0.1),
+    xticklabels=['One', 'Two', 'Three', 'Four', 'Five'],
+    yticklabels=[f"{100*y:0.1f}%" for y in np.arange(0, 1.1, step=0.1)],
+    spines={'top': False, 'right': False, 'bottom': 'grey', 'left': 'grey'}
+)
+plt.show()
+
+fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(10,8), gridspec_kw={'height_ratios': [1, 3], 'hspace': 0.15})
+sns.lineplot(
+    x='x', y='y', data=test_data, color='grey', err_style=None, linewidth=3, ax=ax[0]
+)
+sns.lineplot(
+    x='x', y='z', data=test_data, color='steelblue', err_style=None, linewidth=5, ax=ax[1]
+)
+ax[0] = csns.customize_axis(
+    ax=ax[0],
+    grid=True,
+    title='Overall Title',
+    ylabel='Upper Y Axis Title',
+    showylabel=True,
+    showxlabel=False,
+    spines={'top': False, 'right': False, 'bottom': False, 'left': 'grey'}
+)
+ax[1] = csns.customize_axis(
+    ax=ax[1],
+    grid=True,
+    ylabel='Lower Y Axis Title'
+)
+plt.show()
+
+fig, ax = plt.subplots(figsize=(10,5))
+sns.barplot(
+    x='d', y='z', data=test_data.iloc[:5, :], color='lightgrey', ax=ax
+)
+ax = csns.customize_axis(
+    ax=ax,
+    grid=True,
+    xticklabels=['Mon', 'Tues', 'Weds', 'Thurs', 'Fri'],
+    spines={'top': False, 'right': False, 'bottom': 'grey', 'left': False}
+)
+ax.patches[0].set_color('steelblue')
+ax.patches[-1].set_color('salmon')
+plt.show()
+
+
+fig, ax = plt.subplots(figsize=(10,8))
+sns.distplot(
+    a=test_data['z'], bins=10, kde=False, norm_hist=False, color='grey', label='Control', ax=ax
+)
+sns.distplot(
+    a=test_data['y'], bins=10, kde=False, norm_hist=False, color='steelblue', label='Test', hist_kws=dict(alpha=0.25), ax=ax
+)
+ax = csns.customize_axis(
+    ax=ax,
+    legend=True
+)
+plt.show()
